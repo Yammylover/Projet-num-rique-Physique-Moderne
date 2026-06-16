1d61e8088bc283267e76591b12ef0f5815548b40
Wenox24 <ibrahimboualaoui@gmail.com>
2026-06-16 16:04:45 +0200

    Maj pour une autre fonction

--- calcul_derive_seconde.py
+++ calcul_derive_seconde.py
@@ -1,7 +1,8 @@
 import numpy as np
+import math
 
 def square(x):
-    return x*x
+    return x*x*4
 
 def double_prime(x):
     return 2*np.ones_like(x) # Création d'un tableau de 2 de la meme taille que x
@@ -19,8 +20,8 @@ h = tab_x[1] - tab_x[0]
 print(f"h = {h:6f}") # Affiche la valeur de h
 
 
-for i in range(1, size - 1):
+for i in range(1, size - 1): # Commence a des bornes décalé a causes des des i+1 et i-1
     d2_algo = (tab_f[i+1] - 2*tab_f[i] + tab_f[i-1]) / (h**2)
-    d2_th   = 2.0  
+    d2_th   = 8.0  
 
     print(f"x = {tab_x[i]:.4f} | f''(algo) = {d2_algo:.6f} | f''(théorie) = {d2_th:.6f} | erreur = {pourcentage_erreur(d2_th, d2_algo):.4f}%")
